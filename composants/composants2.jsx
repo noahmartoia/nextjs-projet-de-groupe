ee2f9d28413422c34846fbf57567824c8abc6da8
noahmartoia <noah.martoia@ecole-isitech.fr>
2024-05-16 14:32:24 +0200

    ajout de confetti quand le compteur arrive a 100

--- composants/composants2.jsx
+++ composants/composants2.jsx
@@ -4,13 +4,27 @@ import { useState } from 'react'
 
 function counter() {
     let [count, setCount] = useState(0)
-    const increment = () => setCount(count+1)
+    const increment = () => {setCount(count+1);
+        if(count >= 99){
+            document.body.style.background='url(https://i.gifer.com/W9k1.gif)'
+        }else{
+            document.body.style.background='white'
+        }
+    }
     const decrement = () => {if(count!==0){
         setCount(count-1);
+        if(count <= 100){
+            document.body.style.background='white'
+        }
     }else{
         alert('on ne peux pas aller dans les negatif');
     }}
-    const reset = () => setCount(count = 0)
+    const reset = () => {
+        setCount(count = 0);
+        if(count < 99){
+            document.body.style.background='white'
+        }
+    }
 
     return <div>
         <p>Compteur : {count}</p>
